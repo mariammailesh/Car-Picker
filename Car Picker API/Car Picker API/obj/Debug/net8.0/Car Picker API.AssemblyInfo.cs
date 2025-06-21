d0aa0993a2be1f51e5c2bcd68f57b73edf98db11
Gofran Alkesbah <gofranalkesbah@gmail.com>
2025-06-22 01:33:57 +0300

    fixed conflict

--- Car Picker API/Car Picker API/obj/Debug/net8.0/Car Picker API.AssemblyInfo.cs
+++ Car Picker API/Car Picker API/obj/Debug/net8.0/Car Picker API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car Picker API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ab72c9da8c733be51843718a12dd30c017c0a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7713a7a2e0bd73f535aff1752701d6a255cbd33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car Picker API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car Picker API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
