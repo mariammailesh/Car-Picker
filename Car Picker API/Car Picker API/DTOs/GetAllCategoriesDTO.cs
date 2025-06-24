804a076fcb96512a5f8071f70774747c0a1474d2
Gofran Alkesbah <gofranalkesbah@gmail.com>
2025-06-24 17:19:53 +0300

    Add Get All Categories end point

---
+++ Car Picker API/Car Picker API/DTOs/GetAllCategoriesDTO.cs
@@ -0,0 +1,18 @@
+﻿using Car_Picker_API.Helpers.Enums;
+
+namespace Car_Picker_API.DTOs
+{
+    public class GetAllCategoriesDTO
+    {
+        public int Id { get; set; }
+        public string OfficeName { get; set; }
+        public int ReservationsCount { get; set; }
+        public string OfficeCategory { get; set; } 
+        public string OfficeAddress { get; set; }
+        public string OfficePhoneNumber { get; set; }
+        public string OfficeEmail { get; set; }
+        public string OfficeDescription { get; set; }
+        public string OfficeImageUrl { get; set; }
+
+    }
+}
---
+++ Car Picker API/Car Picker API/Interfaces/ICategoriesInterface.cs
@@ -0,0 +1,9 @@
+﻿using Car_Picker_API.DTOs;
+
+namespace Car_Picker_API.Interfaces
+{
+    public interface ICategoriesInterface
+    {
+        Task<List<GetAllCategoriesDTO>> GetAllCategories();
+    }
+}
