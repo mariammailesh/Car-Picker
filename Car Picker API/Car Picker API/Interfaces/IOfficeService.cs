644fd232db5b57cdc9f13133e6274e300efea048
Gofran Alkesbah <gofranalkesbah@gmail.com>
2025-07-02 00:02:22 +0300

    Update 8

--- Car Picker API/Car Picker API/Interfaces/IOfficeService.cs
+++ Car Picker API/Car Picker API/Interfaces/IOfficeService.cs
@@ -6,14 +6,10 @@ namespace Car_Picker_API.Interfaces
     public interface IOfficeService
     {
         Task<List<OfficeDTO>> GetAllOfficesAsync();
-        Task<List<OfficeDTO>> GetOfficesByCategoryAsync(OfficesCategory category);
-<<<<<<< Updated upstream
 
         Task<List<GetOfficeInfoDTO>> GetOfficesInfo();
 
 
-=======
-       Task<List<GetOfficeInfoDTO>> GetOfficesInfo();
->>>>>>> Stashed changes
+        Task<List<GetOfficeInfoDTO>> GetOfficesInfo();
     }
 }
